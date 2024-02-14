a25e538f9d0610aaa690b1ede00f4a89349c33f9
OmarEP <omrspz@gmail.com>
2024-02-14 00:22:07 -0500

    Added a new handler for the root path '/'

--- main.go
+++ main.go
@@ -1,31 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	// Create a new http.ServeMux
 	mux := http.NewServeMux()
-
-	// Wrap that mux in a custom middleware function that adds 
-	// CORS headers to the response (see the tip below on how to do that).
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 	corsMux := middlewareCors(mux)
 
-	// Create a new http.Server and use the corsMux as the handler
-	server := &http.Server{
-		Addr: "localhost:8080",
+	srv := &http.Server{
+		Addr:    "localhost:8080",
 		Handler: corsMux,
 	}
-	// Use the server's ListenAndServe method to start the server
-	server.ListenAndServe()
-	// Build and run your server (e.g. go build -o out && ./out)
-	
-	// Open http://localhost:8080 in your browser. You should see a 404 error because we haven't connected any handler logic yet. 
-	// Don't worry, that's what is expected for the tests to pass for now.
 
-	// Paste the URL of your web server (e.g. http://localhost:8080)
-	// into the text box and run the HTTP tests.
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	srv.ListenAndServe()
+
 }
 
 func middlewareCors(next http.Handler) http.Handler {
